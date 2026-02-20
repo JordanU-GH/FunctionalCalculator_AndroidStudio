85cbc21ed9f72df784ae1f451e8c9855dc3975b5
JordanU-GH <junderwood9@stu.jsu.edu>
2026-02-19 19:08:39 -0600

    Imported code to create layout dynamically; adapted current code to support MVC pattern; created foundation for button functionality and state machine operability.

--- app/src/main/java/edu/jsu/mcis/cs408/functionalcalculator/DefaultModel.java
+++ app/src/main/java/edu/jsu/mcis/cs408/functionalcalculator/DefaultModel.java
@@ -2,19 +2,36 @@ package edu.jsu.mcis.cs408.functionalcalculator;
 
 import android.util.Log;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 public class DefaultModel extends AbstractModel {
-
     public static final String TAG = "DefaultModel";
-
-    /*
-     * This is a simple implementation of an AbstractModel which encapsulates
-     * two text fields, text1 and text2, which (in this example) are each
-     * reflected in the View as an EditText field and a TextView label.
-     */
-
-    private String text1;
-    private String text2;
-
+    private String DisplayText;
+    private BigDecimal LeftOperand;
+    private BigDecimal RightOperand;
+    private Operator CurrentOperator;
+    private CalculatorState CurrentState;
+    public enum CalculatorState{
+        CLEAR{ @Override public CalculatorState nextState(){ return LHS; } },
+        LHS{ @Override public CalculatorState nextState(){ return OP_SCHEDULED; } },
+        OP_SCHEDULED{ @Override public CalculatorState nextState(){ return RHS; } },
+        RHS{ @Override public CalculatorState nextState(){ return RESULT; } },
+        RESULT{ @Override public CalculatorState nextState(){ return LHS; } },
+        ERROR{ @Override public CalculatorState nextState(){ return CLEAR; } };
+        public abstract CalculatorState nextState();
+    }
+    public enum Operator{
+        ADD{public BigDecimal compute(BigDecimal L, BigDecimal R){ return L.add(R, MathContext.DECIMAL64); } },
+        SUB{public BigDecimal compute(BigDecimal L, BigDecimal R){ return L.subtract(R, MathContext.DECIMAL64); } },
+        MULT{public BigDecimal compute(BigDecimal L, BigDecimal R){ return L.multiply(R, MathContext.DECIMAL64); } },
+        DIV{public BigDecimal compute(BigDecimal L, BigDecimal R){ return L.divide(R, MathContext.DECIMAL64); } },
+        NEG{public BigDecimal compute(BigDecimal operand){ return operand.negate(); } },
+        SQRT{public BigDecimal compute(BigDecimal operand){ return operand.sqrt(MathContext.DECIMAL64); } },
+        PERC{public BigDecimal compute(BigDecimal L, BigDecimal R){ return L.multiply(R).divide(new BigDecimal( 100), MathContext.DECIMAL64); } },
+        NONE;
+    }
     /*
      * Initialize the model elements to known default values.  We use the setter
      * methods instead of changing the values directly so that these changes are
@@ -24,51 +41,54 @@ public class DefaultModel extends AbstractModel {
 
     public void initDefault() {
 
-        setText1("Sample Text 1");
-        setText2("Sample Text 2");
-
+        setDisplayText(new BigDecimal(0).toString());
+        setCurrentOperator(Operator.NONE);
+        setCurrentState(CalculatorState.CLEAR);
+        setLeftOperand(new BigDecimal(0));
+        setRightOperand(null);
     }
 
     /*
      * Simple getter methods for text1 and text2
      */
 
-    public String getText1() {
-        return text1;
+    public String getDisplayText() {
+        return this.DisplayText;
     }
-
-    public String getText2() {
-        return text2;
-    }
+    public Operator getCurrentOperator(){ return this.CurrentOperator; }
+    public CalculatorState getCurrentState(){ return this.CurrentState; }
+    public BigDecimal getLeftOperand(){ return this.LeftOperand; }
+    public BigDecimal getRightOperand(){ return this.RightOperand; }
 
     /*
-     * Setters for text1 and text2.  Notice that, in addition to changing the
+     * Setter for the display.  Notice that, in addition to changing the
      * values, these methods announce the change to the controller by firing a
      * PropertyChange event.  Any registered AbstractController subclasses will
      * receive this event, and will propagate it to all registered Views so that
      * they can update themselves accordingly.
      */
 
-    public void setText1(String newText) {
+    public void setDisplayText(String newVal) {
 
-        String oldText = this.text1;
-        this.text1 = newText;
+        String oldVal = this.DisplayText;
+        this.DisplayText = newVal;
 
-        Log.i(TAG, "Text1 Change: From " + oldText + " to " + newText);
+        Log.i(TAG, "DisplayText Change: From " + oldVal + " to " + newVal);
 
-        firePropertyChange(DefaultController.ELEMENT_TEXT1_PROPERTY, oldText, newText);
-
-    }
-
-    public void setText2(String newText) {
-
-        String oldText = this.text2;
-        this.text2 = newText;
-
-        Log.i(TAG, "Text2 Change: From " + oldText + " to " + newText);
-
-        firePropertyChange(DefaultController.ELEMENT_TEXT2_PROPERTY, oldText, newText);
+        firePropertyChange(DefaultController.ELEMENT_DISPLAY_PROPERTY, oldVal, newVal);
 
     }
 
+    public void setCurrentOperator(Operator newOp) {
+        this.CurrentOperator = newOp;
+    }
+    public void setCurrentState(CalculatorState newState){
+     this.CurrentState = newState;
+    }
+    public void setRightOperand(BigDecimal newVal){
+        RightOperand = newVal;
+    }
+    public void setLeftOperand(BigDecimal newVal){
+        LeftOperand = newVal;
+    }
 }
--- app/src/main/java/edu/jsu/mcis/cs408/functionalcalculator/MainActivity.java
+++ app/src/main/java/edu/jsu/mcis/cs408/functionalcalculator/MainActivity.java
@@ -2,22 +2,25 @@ package edu.jsu.mcis.cs408.functionalcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.beans.PropertyChangeEvent;
 
 import edu.jsu.mcis.cs408.functionalcalculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements AbstractView {
-
     public static final String TAG = "MainActivity";
-
     private ActivityMainBinding binding;
-
     private DefaultController controller;
+    private int DisplayID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,93 +31,159 @@ public class MainActivity extends AppCompatActivity implements AbstractView {
         setContentView(view);
 
         /* Create Controller and Model */
-
         controller = new DefaultController();
         DefaultModel model = new DefaultModel();
 
         /* Register Activity View and Model with Controller */
-
         controller.addView(this);
         controller.addModel(model);
 
+        /* Initialize the Layout of the Calculator*/
+        initLayout();
+
         /* Initialize Model to Default Values */
-
         model.initDefault();
-
-        /* Associate Click Handler with Input Buttons */
-
-        DefaultClickHandler click = new DefaultClickHandler();
-        ConstraintLayout layout = binding.layout;
-
-        for (int i = 0; i < layout.getChildCount(); ++i) {
-            View child = layout.getChildAt(i);
-            if(child instanceof Button) {
-                child.setOnClickListener(click);
-            }
-        }
-
     }
 
     @Override
     public void modelPropertyChange(final PropertyChangeEvent evt) {
-
         /*
          * This method is called by the "propertyChange()" method of AbstractController
          * when a change is made to an element of a Model.  It identifies the element that
          * was changed and updates the View accordingly.
          */
-
         String propertyName = evt.getPropertyName();
         String propertyValue = evt.getNewValue().toString();
 
         Log.i(TAG, "New " + propertyName + " Value from Model: " + propertyValue);
 
-        if ( propertyName.equals(DefaultController.ELEMENT_TEXT1_PROPERTY) ) {
-
-            String oldPropertyValue = binding.outputText1.getText().toString();
-
-            if ( !oldPropertyValue.equals(propertyValue) ) {
-                binding.outputText1.setText(propertyValue);
-            }
-
+        if ( propertyName.equals(DefaultController.ELEMENT_DISPLAY_PROPERTY) ) {
+            TextView display = (TextView) binding.getRoot().getViewById(DisplayID);
+            display.setText(propertyValue);
         }
-
-        else if ( propertyName.equals(DefaultController.ELEMENT_TEXT2_PROPERTY) ) {
-
-            String oldPropertyValue = binding.outputText2.getText().toString();
-
-            if ( !oldPropertyValue.equals(propertyValue) ) {
-                binding.outputText2.setText(propertyValue);
-            }
-
-        }
-
     }
 
-    class DefaultClickHandler implements View.OnClickListener {
-
+    class CalculatorClickHandler implements View.OnClickListener {
         @Override
-        public void onClick(View v) {
+        public void onClick(View view) {
+            String tag = view.getTag().toString();
+            Toast toast = Toast.makeText(binding.getRoot().getContext(), tag, Toast.LENGTH_SHORT);
+            toast.show();
+            // Determine what to do based on which button was pressed
+            // number buttons
+            // operator buttons
+        }
+    }
 
-            /*
-             * When the "Change" buttons are clicked, inform the controller of an input field
-             * change, so that the Model(s) can be updated accordingly.
-             */
+    // Method to create the applications' views (buttons,labels,etc.) programmatically
+    private void initLayout(){
+        // variables that are used to define the number of buttons on the calculator
+        final int BUTTON_COLUMNS = 5;
+        final int BUTTON_ROWS = 4;
+        // Constants used for attributes
+        final int BUTTON_TEXT_SIZE = 24;
+        final int DISPLAY_TEXT_SIZE = 48;
+        final int BUTTON_LEFT_CONSTRAINT_MARGIN = 8;
+        final int BUTTON_TOP_CONSTRAINT_MARGIN = 8;
 
-            String tag = v.getTag().toString();
+        // obtain a reference to the ConstraintLayout being used by the application
+        ConstraintLayout layout = binding.main;
+        // Obtain id's for our guidelines
+        int northID = binding.guideNorth.getId();
+        int eastID = binding.guideEast.getId();
+        int southID = binding.guideSouth.getId();
+        int westID = binding.guideWest.getId();
+        // Create 2D arrays to define ordering of buttons by view id
+        int [][] horizontals = new int [BUTTON_ROWS][BUTTON_COLUMNS];
+        int [][] verticals = new int [BUTTON_COLUMNS][BUTTON_ROWS];
 
-            if (tag.equals("inputButton1")) {
-                String newText = binding.inputText1.getText().toString();
-                controller.changeElementText1(newText);
+        // Create a new constraint set to be configured to our needs
+        ConstraintSet set = new ConstraintSet();
+        set.clone(layout);
+
+        // Create the calculator's display, set its attributes, and add it to the layout
+        String displayName = getResources().getString(R.string.display_name);
+        String displayText = getResources().getString(R.string.display_default_value);
+        int displayID = View.generateViewId();
+        TextView display = new TextView(this);
+        this.DisplayID = displayID;
+        customizeDisplay(display, displayID, displayName, displayText, DISPLAY_TEXT_SIZE);
+        layout.addView(display);
+
+        // add constraints to our display
+        set.constrainWidth(displayID, ConstraintSet.MATCH_CONSTRAINT);
+        set.constrainHeight(displayID, ConstraintSet.WRAP_CONTENT);
+        set.connect(displayID, ConstraintSet.TOP, northID, ConstraintSet.BOTTOM, 0);
+        set.connect(displayID, ConstraintSet.LEFT, westID, ConstraintSet.RIGHT, 0);
+        set.connect(displayID, ConstraintSet.RIGHT, eastID, ConstraintSet.LEFT, 0);
+
+        // Create a new clickhandler for button logic
+        CalculatorClickHandler click = new CalculatorClickHandler();
+        // Get references to our string-array resources for our buttons' names and faces
+        String [] names = getResources().getStringArray(R.array.btn_names);
+        String [] faces = getResources().getStringArray(R.array.btn_faces);
+        // nested for-loops to create buttons, set their attributes, add them to the layout, add their constraints, and populate our 2D arrays
+        for (int row = 0; row < BUTTON_ROWS; row++){
+            for (int col = 0; col < BUTTON_COLUMNS; col++){
+                int index = (BUTTON_COLUMNS * row) + col;
+                int id = View.generateViewId();
+                Button btn = new Button(this);
+                customizeBtn(btn, id, index, names, faces, BUTTON_TEXT_SIZE);
+                layout.addView(btn);
+                // Set an onClickListener for each button
+                btn.setOnClickListener(click);
+                horizontals[row][col] = id;
+                verticals[col][row] = id;
+                // Adding constraints
+                // Horizontal constraints
+                if (col == 0) {
+                    set.connect(id, ConstraintSet.LEFT, westID, ConstraintSet.LEFT, 0);
+                }
+                else if (col == BUTTON_COLUMNS - 1){
+                    set.connect(id, ConstraintSet.RIGHT, eastID, ConstraintSet.RIGHT, 0);
+                    set.connect(id, ConstraintSet.LEFT, horizontals[row][col-1], ConstraintSet.LEFT, BUTTON_LEFT_CONSTRAINT_MARGIN);
+                }
+                else{
+                    set.connect(id, ConstraintSet.LEFT, horizontals[row][col-1], ConstraintSet.LEFT, BUTTON_LEFT_CONSTRAINT_MARGIN);
+                }
+                // Vertical constraints
+                if (row == 0) {
+                    set.connect(id, ConstraintSet.TOP, displayID, ConstraintSet.BOTTOM, 0);
+                }
+                else if (col == BUTTON_ROWS - 1){
+                    set.connect(id, ConstraintSet.BOTTOM, southID, ConstraintSet.BOTTOM, 0);
+                    set.connect(id, ConstraintSet.TOP, horizontals[row-1][col], ConstraintSet.TOP, BUTTON_TOP_CONSTRAINT_MARGIN);
+                }
+                else{
+                    set.connect(id, ConstraintSet.TOP, horizontals[row-1][col], ConstraintSet.TOP, BUTTON_TOP_CONSTRAINT_MARGIN);
+                }
             }
-
-            else if (tag.equals("inputButton2")) {
-                String newText = binding.inputText2.getText().toString();
-                controller.changeElementText2(newText);
-            }
-
         }
 
+        // Iterate through our 2D arrays to build chains to arrange the buttons
+        for (int [] viewIds : horizontals) {
+            set.createHorizontalChain(westID, ConstraintSet.LEFT, eastID, ConstraintSet.RIGHT, viewIds, null, ConstraintSet.CHAIN_PACKED);
+        }
+        for (int [] viewIds : verticals){
+            set.createVerticalChain(displayID, ConstraintSet.BOTTOM, southID, ConstraintSet.TOP, viewIds, null, ConstraintSet.CHAIN_PACKED);
+        }
+
+        // apply our ConstraintSet to the Layout
+        set.applyTo(layout);
+    }
+
+    private void customizeDisplay(TextView display, int id, String name, String text, int txtSize){
+        display.setId(id);
+        display.setTag(name);
+        display.setText(text);
+        display.setTextSize(txtSize);
+        display.setGravity(Gravity.CENTER_VERTICAL | Gravity.END);
+    }
+    private void customizeBtn(Button btn, int id, int index, String [] names, String [] faces, int txtSize){
+        btn.setId(id);
+        btn.setTag(names[index]);
+        btn.setText(faces[index]);
+        btn.setTextSize(txtSize);
     }
 
 }
